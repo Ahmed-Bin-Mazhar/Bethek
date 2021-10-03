a032ed14bf2e0d6621948c7650996eb50b2b3bc7
Ahmed Bin Mazhar <63560564+Ahmed-Bin-Mazhar@users.noreply.github.com>
2021-10-04 02:18:31 +0500

    Update Home.js

--- components/Home.js
+++ components/Home.js
@@ -21,7 +21,7 @@ class Home extends PureComponent {
   };
 
   async componentDidMount() {
-    fetch("http://18.189.188.229:8000/listings/listingsEp/listings-all")
+    fetch("http://3.135.209.144:8000/ep/hostels-all")
       .then((res) => res.json())
       .then((resJson) => {
         this.setState({ Listings: resJson, loading: false });
